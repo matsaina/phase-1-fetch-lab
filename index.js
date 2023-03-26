7c920fb82ad76c7e18c34fada3147a81e2e11f4d
matsaina <matsaina@gmail.com>
2023-03-26 23:51:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json[0]));
+
+  // (json['0'].name));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
